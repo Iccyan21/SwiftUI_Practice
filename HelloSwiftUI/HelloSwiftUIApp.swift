2daa0a4b45fd149d8e55f8c8c04c397dfac0fd4a
Iccyan21 <iccyan0429@gmail.com>
2023-12-27 11:21:40 +0900

    Initial Commit

---
+++ HelloSwiftUI/HelloSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloSwiftUIApp.swift
+//  HelloSwiftUI
+//
+//  Created by 水原　樹 on 2023/12/27.
+//
+
+import SwiftUI
+
+@main
+struct HelloSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
